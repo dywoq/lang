baf9cc0a5263304f66d67a942fcf54a27570647c
dywoq <aleks.koyf@gmail.com>
2025-10-27 10:49:01 +0300

    pkg/cli: add command.go

---
+++ pkg/cli/command.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+)
+
+// Command represents the CLI command,
+// with the name and execute method.
+type Command interface {
+	Name() string
+	Execute() error
+}
+
+var commands map[string]Command
+
+// RegisterCommand registers the command into
+// the underlying map.
+// Does nothing if command already exists.
+func RegisterCommand(c Command) {
+	if _, ok := commands[c.Name()]; ok {
+		return
+	}
+	commands[c.Name()] = c
+}
+
+// ExecuteCommand executes the command,
+// whose name is equal to os.Args[1].
+//
+// Otherwise, the error is returned
+// if command couldn't be found.
+func ExecuteCommand() error {
+	cmd, ok := commands[os.Args[1]]
+	if !ok {
+		return fmt.Errorf("cli: unknown command: %s", os.Args[1])
+	}
+	return cmd.Execute()
+}
