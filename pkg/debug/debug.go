780ee313d8e74000255c871272eaf297906e8625
dywoq <aleks.koyf@gmail.com>
2025-12-09 07:51:00 +0300

    pkg/debug: add Debug structure
    Debug structure includes these features:
    - No race-data: Using atomic.Bool ensure one or few goroutines can use this flag
    
    - log.Logger: You can set own flags for logger
    
    - Safe: No one can accidentally change the output without SetOutput

--- pkg/debug/debug.go
+++ pkg/debug/debug.go
@@ -13,3 +13,47 @@
 // limitations under the License.
 
 package debug
+
+import (
+	"io"
+	"log"
+	"sync/atomic"
+)
+
+type Debug struct {
+	output io.Writer
+	busy   *atomic.Bool
+	logger *log.Logger
+}
+
+// New creates new [Debug] structure and returns a pointer of it.
+func New(output io.Writer, busy *atomic.Bool) *Debug {
+	l := log.New(output, "", 0)
+	return &Debug{output: output, busy: busy, logger: l}
+}
+
+// SetOutput sets the output, but panics if it's busy.
+func (d *Debug) SetOutput(output io.Writer) {
+	if d.busy.Load() {
+		panic("can't modify debug parameters when it's busy")
+	}
+	d.output = output
+	d.logger.SetOutput(output)
+}
+
+// Output returns the set output instance.
+func (d *Debug) Output() io.Writer {
+	return d.output
+}
+
+func (d *Debug) Println(v ...any) {
+	d.logger.Println(v...)
+}
+
+func (d *Debug) Printf(fmt string, v ...any) {
+	d.logger.Printf(fmt, v...)
+}
+
+func (d *Debug) SetFlags(flags int) {
+	d.logger.SetFlags(flags)
+}
